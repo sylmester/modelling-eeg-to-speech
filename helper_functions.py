8c4e34a5cd6838a51815ff684eada5f545b8539c
Sylvester <90195024+sylmester@users.noreply.github.com>
2026-05-11 22:29:41 +0200

    Added Alice with statistics following implemented

--- helper_functions.py
+++ helper_functions.py
@@ -425,6 +425,31 @@ def alice_get_durations(envelope, STIMULI):
     durations = [gt.time.tmax for stimulus, gt in zip(STIMULI, envelope)]
     return durations
 
+def alice_get_models(exclude=[]):
+    envelopes_log = eelbrain.load.unpickle(ALICE_PROCESSED_PREDICTOR_DIR / f'~processed_envelopes-log.pickle')
+    envelopes_onset = eelbrain.load.unpickle(ALICE_PROCESSED_PREDICTOR_DIR / f'~processed_envelopes-onset.pickle')
+
+    models = {
+    'envelope_log': [envelopes_log],
+    'envelope_onset': [envelopes_onset],
+    # Compare different scales for the acoustic response
+    #'envelope_log_8band': [envelopes_log_8band],
+    # The acoustic edge detection model
+    'envelope_log_onset': [envelopes_log, envelopes_onset],
+    #'envelope_onset_8band': [envelopes_onset_8band],
+    #'acoustic_8band': [envelopes_log_8band, envelopes_onset_8band],
+    # Models with word-onsets and word-class
+    #'words': [envelopes_words_onset],
+    #'words+lexical': [envelopes_words_onset, envelopes_words_lexical, envelopes_words_nlexical],
+    #'acoustic+words': [envelopes_log_8band, envelopes_onset_8band, envelopes_words_onset],
+    #'acoustic+words+lexical': [envelopes_log_8band, envelopes_onset_8band, envelopes_words_onset, 
+    #                           envelopes_words_lexical, envelopes_words_nlexical],
+}
+    if exclude != []:
+        for exclution in exclude:
+            models.pop(exclution)
+    return models
+
 
 # =============================================================================
 # PLOTTING FUNCTIONS
